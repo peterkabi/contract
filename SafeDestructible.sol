e79ed1a63f8895d914a9f0f8ff496e93c33be1f1
Peter kabi <41149134+peterkabi@users.noreply.github.com>
2020-02-19 11:19:59 +0300

    Add files via upload

---
+++ SafeDestructible.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/ownership/Ownable.sol";
+
+
+/**
+ * @title SafeDestructible
+ * Base contract that can be destroyed by owner.
+ * Can be destructed if there are no funds on contract balance.
+ */
+contract SafeDestructible is Ownable {
+    function destroy() onlyOwner public {
+        require(address(this).balance == 0);
+        selfdestruct(owner);
+    }
+}
+
